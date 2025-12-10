470ad85d4d699114ba6fe1e5132542c14b1e9634
dhaanu-quest <dhaanu97@gmail.com>
2025-12-11 00:01:55 +0530

    port

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ app.use((err, req, res, next) => {
 });
 
 // Start server (only in development)
-const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV !== 'production') {
+const PORT = 5000
+// if (process.env.NODE_ENV !== 'production') {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-}
+// }
 
 // Export for Vercel
 module.exports = app;
